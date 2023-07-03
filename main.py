61454323e1d5e65e110cc69ba56a8b960fb4c7f2
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2023-07-03 12:32:38 +0600

    More Modifications

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app=FastAPI()
 
-@app.get("/")
+@app.get("/")#GET REQUEST
 def root():
     return {"message":"Hello Sivan!"}
+
+@app.post("/createposts")
+def create_posts(payLoad: dict= Body(...)):
+    print(payLoad)
+    return {"new_post":f"title {payLoad['title']} content: {payLoad['content']}"}
